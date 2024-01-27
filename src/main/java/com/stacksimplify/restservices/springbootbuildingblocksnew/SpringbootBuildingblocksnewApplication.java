2217eb0fd17238dc4a0dc48bde07114831258cdb
Ryan Travasso <travassor@vmware.com>
2024-01-27 23:22:18 +0530

    first commit

---
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocksnew/SpringbootBuildingblocksnewApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.springbootbuildingblocksnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplify/restservices/springbootbuildingblocksnew/SpringbootBuildingblocksnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.springbootbuildingblocksnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
